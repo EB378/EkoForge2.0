bc0f14be9337000b4822a8ab8fb9262fa3a22100
EAlexBen <107051871+EB378@users.noreply.github.com>
2024-12-17 22:05:11 +0000

    modified:   i18n.ts 	modified:   middleware.ts 	modified:   netlify.toml 	modified:   next.config.mjs 	modified:   package-lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,7 @@
 
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin({
-  // Path to your i18n request configuration file
-  localePrefix: "as-needed", // Add locale prefixes only when needed
-  messagesDir: "./messages", // Path where your translation files are stored
-});
-
-const nextConfig = {
-  // Add your Next.js configuration here
-  i18n: {
-    locales: ["en", "fi"], // Supported locales
-    defaultLocale: "en",   // Default locale
-  },
-  experimental: {
-    appDir: true, // Ensure app directory is enabled for the App Router
-  },
-};
+const withNextIntl = createNextIntlPlugin();
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
